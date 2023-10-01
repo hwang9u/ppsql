d13db9a4eca6d3e43e86ff524be52f218bf23960
hwang9u <hwang9u@gmail.com>
2023-10-02 01:03:21 +0900

    Update core.py
    Add comment

--- core.py
+++ core.py
@@ -1,4 +1,5 @@
-# Python PostgreSQL
+# PPSQL: Python PostgreSQL
+
 import psycopg2 as pg 
 import psycopg2.extras as ex
 import pandas as pd
@@ -10,27 +11,42 @@ def get_connection(db_configs):
     return conn, cur
 
 def _get_colnames(cur):
-    '''
+    """
     cur.description을 통해 table 열 이름 반환
     
     Args:
         cur: DB가 연결된 cursor
     
     Returns:
-        (list): 열 이름
-    '''
+        colnames(list): 열 이름
+    """
     colnames = [desc[0] for desc in cur.description]
     return colnames
 
 def check_punc(query):
+    """
+    쿼리가 ";" 형태로 끝나는지 확인 후 없으면 마침 표식을 추가
+    
+    Args:
+        query(str): 쿼리
+    
+    Returns:
+        (str): 마침표가 확인/추가된 쿼리
+    """
     return query if query.strip().endswith(';') else query + ';'
 
 
 def _select(query, cur, n=-1):
-    '''
-    query(str): PostgreSQL query
-    cur
-    '''
+    """
+    SELECT 쿼리 실행 결과
+    
+    Args:
+        query(str): 쿼리
+        n(int): 출력할 결과의 수, -1이면 전체 출력
+    
+    Returns:
+        (list) 쿼리 실행 결과
+    """
     try:
         assert isinstance(n, int)
     except:
@@ -48,37 +64,59 @@ def _select(query, cur, n=-1):
     colnames = _get_colnames(cur)
     return result, colnames
     
+    
 def commit_query(query, cur, conn, values=None):
+    """
+    conn.commit()이 필요한 쿼리 실행 함수
+
+    Args:
+        query (str): 쿼리
+        cur (psycopg2.extensions.cursor): 연결된 cursor 객체
+        conn (psycopg2.extensions.connection): 연결된 connection 객체
+        values (tuple, optional): placeholder 활용할 때 value Defaults to None.
+    """
     query = check_punc(query)
     cur.execute(query, values)
     conn.commit()
 
-def pandas2tuples(pandas_df):
-    '''
-    pandas data frame -> tuple 형태로 변환.
+def pandas2tuples(df):
+    """
+    pandas data frame을 tuple 형태로 변환.
     tolist()를 통해서 numpy.dtype을 python native 형태로 변환
     
     Args:
-        pandas_df(pandas.DataFrame): 변환할 pandas data frame
+        df(pandas.DataFrame): 변환할 pandas data frame
         
     Returns:
         (list): 변환된 tuple list
-    '''
-    return [ tuple(x.tolist()) for x in pandas_df.values]
+    """
+    return [tuple(x.tolist()) for x in df.values]
 
 def insert_from_tuples(query, tuples, cursor, conn):
-    '''
+    """
+    tuple list를 입력으로 하여 table에 insert 수행
+
     Args:
-        : pandas data frame
-        
-    '''
+        query (str): _description_
+        tuples (list): tuple들의 list 형태
+        cursor (psycopg2.extensions.cursor): 연결된 cursor 객체
+        conn (psycopg2.extensions.connection): 연결된 connection 객체
+    """
     query = check_punc(query)
     pg.extras.execute_values(cursor, query, tuples, template=None, page_size=1000)
     conn.commit()
     
     
 class PyPostgreSql:
+    """
+    Python과 PostgreSQL 연동 클래스
+    """
     def __init__(self, config, verbose=True):
+        """
+        Args:
+            config (dict): DB 연결을 위한 config
+            verbose (bool, optional): config 출력 여부. Defaults to True.
+        """
         self.config = config
         self.conn = pg.connect(**config)
         self.cur = self.conn.cursor()
@@ -88,6 +126,17 @@ class PyPostgreSql:
             
             
     def select(self, query, n=-1, return_type='tuple'):
+        """
+        SELECT 수행
+
+        Args:
+            query (str): 쿼리(SELECT)
+            n (int, optional): 반환할 결과의 개수. Defaults to -1. 만약 -1이면 전체 결과 반환
+            return_type (str, optional): 반환 형태 'pandas'와 'tuple' 중 선택. Defaults to 'tuple'.
+
+        Returns:
+            (pandas.DataFrame or list ): SELECT 수행 결과. return_type에 따라 반환 형태 결정
+        """
         result,colnames = _select(query, self.cur, n)
         
         if return_type == 'tuple': 
@@ -96,12 +145,24 @@ class PyPostgreSql:
             return pd.DataFrame(result, columns=colnames)
 
     def commit(self, query, values=None):
-        '''
-        create, insert, update, delete
-        '''
+        """
+        CREATE, INSERT, DELETE, UPDATE... 관련 명령 수행
+
+        Args:
+            query (str): 쿼리(CREATE, INSERT, DELETE, UPDATE, ...)
+            values (tuple, optional): placeholder 형태로 명령 수행 시 값. Defaults to None.
+        """
         commit_query(query, values=values, cur=self.cur, conn=self.conn)
     
     def insert(self, query, tuples=None, df=None):
+        """
+        tuple을 입력으로 하여 INSERT 수행
+
+        Args:
+            query (str): 쿼리(INSERT)
+            tuples (list, optional): 추가할 값(tuple list). Defaults to None.
+            df (pandas.DataFrame, optional): 추가할 값(data frame). Defaults to None.
+        """
         if (not isinstance(df, type(None))) & isinstance(tuples, type(None)):
             tuples = pandas2tuples(df)
             
@@ -109,10 +170,19 @@ class PyPostgreSql:
             
             
     def print_db_config(self):
+        """
+        config 출력 함수
+        """
         print("""
-                DB address: {}\n
-                DB name: {}\n
-              """.format(DB_ADDRESS, DB_NAME))
+              DB connection config
+              ---------------------------------
+              DB address: {host}
+              DB name: {dbname}
+              user: {user}
+              password: {password}
+              port: {port}
+              ---------------------------------
+              """.format(**self.config))
     
     def close_conn(self):
         self.cur.close()
@@ -122,9 +192,7 @@ class PyPostgreSql:
 
 
 if __name__ == '__main__':
-    import pandas as pd
-    
-    # Configs for DB connection
+    # Define Configs for DB connection
     DB_ADDRESS = "localhost"
     DB_NAME = "analysis"
     USER = 'postgres'
@@ -159,12 +227,10 @@ if __name__ == '__main__':
     pp.insert(query='INSERT INTO test_table (id, name) VALUES %s', tuples=test_tuple)
     print(pp.select('select * from test_table', return_type='pandas'))
     
-    
     # (3) Delete
     print('After deletion')
     pp.commit("DELETE FROM test_table WHERE id=1")
     print(pp.select('select * from test_table', return_type='pandas'))
-    
     
     # (4) Update
     # pp.commit("UPDATE test_table set name='망' WHERE name='mang' ") # query type
